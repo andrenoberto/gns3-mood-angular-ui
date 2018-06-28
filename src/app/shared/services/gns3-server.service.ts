52db706d65fc54d22bd4190ea80222b812621943
Andre Noberto <joao.andre.souza@accenture.com>
2018-06-28 07:49:52 -0300

    Added new endpoints

--- src/app/shared/services/gns3-server.service.ts
+++ src/app/shared/services/gns3-server.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {environment} from '../../../environments/environment';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class Gns3ServerService {
@@ -17,7 +18,32 @@ export class Gns3ServerService {
     return this.http.post(environment.apiRootUrl + '/projects', body, this.httpOptions);
   }
 
+  openProject(projectId): Observable<any> {
+    return this.http.post(environment.apiRootUrl + '/projects/' + projectId + '/open', {}, this.httpOptions);
+  }
+
+  closeProject(projectId): Observable<any> {
+    return this.http.post(environment.apiRootUrl + '/projects/' + projectId + '/close', {}, this.httpOptions);
+  }
+
+  createNode(projectId, name, node_type, compute_id): Observable<any> {
+    const body = {
+      name,
+      node_type,
+      compute_id
+    };
+    return this.http.post(environment.apiRootUrl + '/projects' + projectId + '/nodes', body, this.httpOptions);
+  }
+
   getProjectList(): Observable<any> {
     return this.http.get(environment.apiRootUrl + '/projects', this.httpOptions);
+  }
+
+  getOpenedProjects(): Observable<any> {
+    return this.getProjectList().pipe(
+      map(_r => {
+        return _r.filter(_res => _res.status === 'opened');
+      })
+    );
   }
 }
