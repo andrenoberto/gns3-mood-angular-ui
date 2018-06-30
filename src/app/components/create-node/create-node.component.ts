e7996457866d893b6a8d942bbb220cbeddd9689c
Andre Noberto <joao.andre.souza@accenture.com>
2018-06-30 11:35:29 -0300

    CreateNode component is now created and working

--- src/app/components/create-node/create-node.component.ts
+++ src/app/components/create-node/create-node.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-create-node',
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-node.component.scss']
 })
 export class CreateNodeComponent implements OnInit {
+  public nodeName: string;
+  public type: string;
 
-  constructor() { }
+  @ViewChild('newNode') el: ElementRef;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
   }
 
+  onOpen() {
+    this.renderer.addClass(this.el.nativeElement, 'is-active');
+  }
+
+  onCloseCancel = () => this.onClose();
+
+  private onClose() {
+    this.renderer.removeClass(this.el.nativeElement, 'is-active');
+    this.clearForm();
+  }
+
+  onSave() {
+    this.onClose();
+  }
+
+  private clearForm() {
+    this.nodeName = '';
+    this.type = '';
+  }
 }
--- src/app/components/create-node/create-node.module.ts
+++ src/app/components/create-node/create-node.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CreateNodeComponent } from './create-node.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CreateNodeComponent} from './create-node.component';
+import {NodeTypeModule} from '../node-type/node-type.module';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    NodeTypeModule
   ],
   declarations: [CreateNodeComponent],
   exports: [CreateNodeComponent]
 })
-export class CreateNodeModule { }
+export class CreateNodeModule {
+}
